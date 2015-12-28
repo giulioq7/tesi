87bfe6b8a78a10f375a136b0bdd086c30fb2026a
giulioq7 <giulio.quarenghi@gmail.com>
2015-12-28 11:28:36 +0100

    nothing

---
+++ SharedClasses/netcomponent.cpp
@@ -0,0 +1,5 @@
+#include "netcomponent.h"
+
+NetComponent::NetComponent()
+{
+}
---
+++ SharedClasses/netcomponent.h
@@ -0,0 +1,10 @@
+#ifndef NETCOMPONENT_H
+#define NETCOMPONENT_H
+
+class NetComponent
+{
+public:
+    NetComponent();
+};
+
+#endif // NETCOMPONENT_H
